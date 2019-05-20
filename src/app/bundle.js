b1eb99f7fb496c5df096cf73e2ed0225b0274734
bo4arovpavel1989 <bo4arovpavel1989@mail.ru>
2019-05-20 21:53:27 +0300

    started add entry form handlers

--- src/app/bundle.js
+++ src/app/bundle.js
@@ -1,3 +1,37 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+class AddEntryHandler {
+  constructor (className) {
+    this.selector = className;
+    this.addListeners();
+  }
 
-},{}]},{},[1]);
+  addListeners () {
+    let forms = document.getElementsByClassName(this.selector)
+
+    for (let i = 0; i < forms.length; i++) {
+       forms[i].querySelector('.addButtons').addEventListener('click', this.handleAddClick)
+       forms[i].querySelector('.closeButton').addEventListener('click', this.handleCloseClick)
+    }
+  }
+
+  handleAddClick (e) {
+    this.parentElement.querySelector('form').classList.toggle('hidden')
+    this.classList.toggle('hidden')
+  }
+
+  handleCloseClick (e) {
+    this.parentElement.parentElement.classList.toggle('hidden')
+    this.parentElement.parentElement.nextElementSibling.classList.toggle('hidden')
+  }
+}
+
+module.exports = AddEntryHandler;
+
+},{}],2:[function(require,module,exports){
+'use strict';
+
+const AddEntryHandler = require('./addentryhandler')
+
+const entryHandler = new AddEntryHandler('entryForm')
+
+},{"./addentryhandler":1}]},{},[2]);
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const AddEntryHandler = require('./addentryhandler')
+
+const entryHandler = new AddEntryHandler('entryForm')
