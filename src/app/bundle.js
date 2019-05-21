0f0981d8046089f0b1f2defc24416d0f162861ce
User <bo4arovpavel1989@mail.ru>
2019-05-21 10:58:06 +0300

    made add entry form handler

--- src/app/bundle.js
+++ src/app/bundle.js
@@ -13,12 +13,31 @@ class AddEntryHandler {
 
       forms[i].querySelector('.addButtons').onclick = this.handleClick.bind(this, form)
       forms[i].querySelector('.closeButton').onclick = this.handleClick.bind(this, form)
+      form.querySelector('form').onsubmit = this.handleSubmit.bind(this, form)
     }
   }
 
   handleClick (form) {
     form.querySelector('.addButtons').classList.toggle('hidden')
     form.querySelector('form').classList.toggle('hidden')
+    form.querySelector('form').reset()
+  }
+
+  handleSubmit (form, e) {
+    e.preventDefault()
+
+    let name = form.querySelector('textarea').value;
+
+    this.addNewEntry(form, name)
+    form.querySelector('form').reset()
+  }
+
+  addNewEntry (form, name) {
+    let entry = document.createElement('div');
+
+    entry.innerHTML = name
+    entry.className = 'entry'
+    form.previousElementSibling.appendChild(entry)
   }
 }
 
