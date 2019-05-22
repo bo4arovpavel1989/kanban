f34bb52daa2b79843ac40fc060794dd7f3f0ad83
User <bo4arovpavel1989@mail.ru>
2019-05-22 15:30:20 +0300

    added jsdocs for dragmanager

--- src/app/bundle.js
+++ src/app/bundle.js
@@ -149,7 +149,13 @@ class AddEntryHandler {
 module.exports = AddEntryHandler
 
 },{"./dragmanager":3}],3:[function(require,module,exports){
+/* Class defining basic drag and drop behavior */
 class DragManager {
+  /**
+   * Method handles behavior on drag start
+   * @param {Object} e - html event ovject
+   * @return {Boolean}
+   */
   dragStart (e) {
     e.target.setAttribute('id', 'isMoved')
     e.dataTransfer.effectAllowed = 'move'
@@ -158,16 +164,31 @@ class DragManager {
     return true;
   }
 
+  /**
+   * Method reset default drag enter behavior
+   * @param {Object} e - html event ovject
+   * @return {Boolean}
+   */
   dragEnter (e) {
     e.preventDefault()
 
     return true;
   }
 
+  /**
+    * Method reset default drag overr behavior
+    * @param {Object} e - html event ovject
+    * @return {void}
+    */
   dragOver (e) {
     e.preventDefault()
   }
 
+  /**
+   * Method defines drop event handler
+   * @param {Object} e - html event object
+   * @return {Boolean}
+   */
   dragDrop (e) {
     let data = e.dataTransfer.getData('Text');
 
